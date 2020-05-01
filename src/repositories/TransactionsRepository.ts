e3eaaea0a9189ad314ee7ff2d122f65dfd3bb092
Jorge Hecherat <hecherat@gmail.com>
2020-05-01 15:26:37 -0300

    Challenges done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,50 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions.reduce((accumulator, transaction) => {
+      return (
+        accumulator + (transaction.type === 'income' ? transaction.value : 0)
+      );
+    }, 0);
+
+    const totalOutcome = this.transactions.reduce(
+      (accumulator, transaction) => {
+        return (
+          accumulator + (transaction.type === 'outcome' ? transaction.value : 0)
+        );
+      },
+      0,
+    );
+
+    const total = totalIncome - totalOutcome;
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    if (type === 'outcome' && this.getBalance().total - value < 0) {
+      throw Error(`Your balance can't be negative!`);
+    }
+
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,32 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (value === 0) {
+      throw Error('The transaction must have a value');
+    }
+
+    if (value < 0 && type === 'income') {
+      throw Error('The income value must be greater then zero');
+    }
+
+    if (value < 0 && type === 'outcome') {
+      throw Error('The outcome value must be greater then zero');
+    }
+
+    if (
+      typeof title === 'string' &&
+      typeof value === 'number' &&
+      (type === 'income' || type === 'outcome')
+    ) {
+      const transaction = this.transactionsRepository.create({
+        title,
+        value,
+        type,
+      });
+      return transaction;
+    }
+    throw Error('Internal error');
   }
 }
 
